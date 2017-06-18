542ab27d28995b842e81f2cc5f984371af87e861
Otis Milliken <otis.true@gmail.com>
2017-06-18 16:42:41 +0000

    Updated About Page and Logo Style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#home' #points to a controller and an action. ex: pages and action home
   get 'about', to: 'pages#about'
 end

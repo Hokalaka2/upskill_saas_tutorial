d18633bd3672bb926aa7132378dab9431ee131ff
Otis Milliken <otis.true@gmail.com>
2017-06-19 00:30:43 +0000

    Added contact for view, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home' #points to a controller and an action. ex: pages and action home
   get 'about', to: 'pages#about'
+  resources :contacts
 end

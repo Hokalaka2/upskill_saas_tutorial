a50fe41cd71e2c1776359b95b22a102533a7c1bd
Otis Milliken <otis.true@gmail.com>
2017-06-23 15:38:09 +0000

    Added costum contact path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home' #points to a controller and an action. ex: pages and action home
   get 'about', to: 'pages#about'
   resources :contacts
+  get 'contact-us', to: 'contacts#new'
 end

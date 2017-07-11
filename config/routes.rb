80e780b1e66c078af916db0d7593bf8af811a789
Otis Milliken <otis.true@gmail.com>
2017-07-11 16:04:06 +0000

    Added server side code for creating subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home' #points to a controller and an action. ex: pages and action home
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
--- db/migrate/20170707185157_add_stripe_costumer_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddStripeCostumerTokenToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :stripe_costumer_token, :string
-  end
-end
